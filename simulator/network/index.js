37fc873082f659e30d35e2d0bf55bbe9933e33df
Timotheus Kampik <timotheus.kampik@gmail.com>
2020-12-27 19:00:40 +0100

    support running multiple simulations with one click

--- simulator/network/index.js
+++ simulator/network/index.js
@@ -6,14 +6,16 @@ const aggParam = url.searchParams.get('agg')
 const fixParam = url.searchParams.get('fix')
 const speParam = url.searchParams.get('spe')
 const attParam = url.searchParams.get('att')
+const mulRunParam = url.searchParams.get('mul')
 
 // configuration settings
 const numberOfNodes = nodParam ? Number(nodParam) : 40
 const hubFixation = fixParam ? Number(fixParam) / 100 : 0.5
 const aggressionLevel = aggParam ? Number(aggParam) / 100 : 0.5
 const numberOfHubs = 3
-const speed = speParam ? 5000 / Number(speParam) : 500
+const speed = speParam ? 2500 / Number(speParam) : 500
 const attackFromHub = attParam === 'true'
+const multipleRuns = mulRunParam === 'true'
 let showAttacker = false
 
 // "file global" object to manage graph library, edges, nodes, hubs, and attackers
@@ -25,9 +27,36 @@ let noHubNodes
 let attacker
 let globalDistances = [...Array(numberOfNodes)]
 const networkHistory = []
+const ticksLimit = 500
+const notificationLimit = 50000
+let ticksCounter = 0
 
 // utils
 const genRandomInt = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min
+
+// initialization function
+const init = () => {
+    cy, edges, nodes, hubs, noHubNodes, attacker = null
+    globalDistances = [...Array(numberOfNodes)]
+    ticksCounter = 0
+    // assign random hubs
+    hubs = Array.from(Array(numberOfHubs)).map(
+        (_, __) => Math.floor(Math.random() * numberOfNodes)
+    )
+    // set attacker
+    if (!attacker) { // determine attacker if this has not been done, yet
+        // if attacker should be hub
+        if (attackFromHub) {
+            attacker = hubs[genRandomInt(0, hubs.length - 1)]
+        } else { // if attacker should not be hub
+            potentialNodes = [...Array(numberOfNodes).keys()]
+            noHubNodes = potentialNodes.filter(id => !hubs.includes(id))
+            attacker = noHubNodes[genRandomInt(0, noHubNodes.length - 1)]
+        }
+        hubs.forEach(hubId => cy.$id(hubId).style({'background-color': '#73A790'}))
+        document.getElementById('show-button').classList.remove('disabled')
+    }
+}
 
 // edge creation functions (helpers)
 const genRandomTarget = source => {
@@ -70,10 +99,6 @@ const getLnRandomNode = (nodeIds, source) => {
     if (id === source) {
         sortedDistances[1].id
     }
-    if (nodeIds.length > 10) {
-        console.log(id)
-        console.log(index)
-    }
     return id
 }
 
@@ -102,16 +127,20 @@ const startSimulation = () => {
         cy.unmount()
         cy.destroy()
     }
-    edges = {}
-    hubs = []
+    const initCy = () => {
+        edges = {}
+        hubs = []
+    
+        nodes = new Array(numberOfNodes).fill({}).map((_, index) => {
+            return {
+                group: 'nodes',
+                data: { id: index },
+                grabbable: false
+            }
+        })
+    }
 
-    nodes = new Array(numberOfNodes).fill({}).map((_, index) => {
-        return {
-            group: 'nodes',
-            data: { id: index },
-            grabbable: false
-        }
-    })
+    initCy()
 
     const render = elements => cytoscape({
         container: document.getElementById('cy'),
@@ -148,24 +177,16 @@ const startSimulation = () => {
     cy.zoomingEnabled(false)
 
     cy.ready(() => {
-        // assign random hubs
-        hubs = Array.from(Array(numberOfHubs)).map(
-            (_, __) => Math.floor(Math.random() * numberOfNodes)
-        )
-        // set attacker
-        if (!attacker) { // determine attacker if this has not been done, yet
-            // if attacker should be hub
-            if (attackFromHub) {
-                attacker = hubs[genRandomInt(0, hubs.length - 1)]
-            } else { // if attacker should not be hub
-                potentialNodes = [...Array(numberOfNodes).keys()]
-                noHubNodes = potentialNodes.filter(id => !hubs.includes(id))
-                attacker = noHubNodes[genRandomInt(0, noHubNodes.length - 1)]
+        init()
+        const interval = setInterval(() => {
+            if(multipleRuns && ticksCounter === ticksLimit) {
+                clearInterval(interval)
+                startSimulation()
             }
-            hubs.forEach(hubId => cy.$id(hubId).style({'background-color': '#73A790'}))
-            document.getElementById('show-button').classList.remove('disabled')
-        }
-        setInterval(() => {
+            if(networkHistory.length === notificationLimit) {
+                alert(`Reached ${notificationLimit} ticks`)
+            }
+            ticksCounter++
             let connection = genRandomConnection()
             const source = connection.source
             const target = connection.target
@@ -179,7 +200,7 @@ const startSimulation = () => {
             if (existingEdge) {
                 const edgeUpdate = {
                     ...existingEdge,
-                    style: { width: existingEdge.style.width + 0.2 }
+                    style: { width: existingEdge.style.width + 0.1 }
                 }
                 edges[existingEdge.data.id] = edgeUpdate
                 cy.$id(existingEdge.data.id).style('width', edgeUpdate.style.width)
@@ -191,15 +212,17 @@ const startSimulation = () => {
                         source: finalConnection.source,
                         target: finalConnection.target
                     },
-                    style: { width: 0.2 }
+                    style: { width: 0.1 }
                 }
                 edges[edgeId] = edge
                 cy.add([edge])
             }
             const historyEntry = { edges, nodes }
             networkHistory.push(historyEntry)
-            const uriContent = `data:text/plain;charset=utf-8,${encodeURIComponent(JSON.stringify(networkHistory))}`
-            document.getElementById('log-button').href = uriContent
+            /*if (ticksCounter % 50 === 0) {
+                const uriContent = `data:text/plain;charset=utf-8,${encodeURIComponent(JSON.stringify(networkHistory))}`
+                document.getElementById('log-button').href = uriContent
+            }*/
         }, speed)
     })
 }
