1897a3fbdb6c87f21280b2e8746d9f088809b456
Timotheus Kampik <timotheus.kampik@gmail.com>
2020-12-27 16:46:59 +0100

    tidy up/improve network simulation

--- simulator/network/index.js
+++ simulator/network/index.js
@@ -5,6 +5,7 @@ const nodParam = url.searchParams.get('nod')
 const aggParam = url.searchParams.get('agg')
 const fixParam = url.searchParams.get('fix')
 const speParam = url.searchParams.get('spe')
+const attParam = url.searchParams.get('att')
 
 // configuration settings
 const numberOfNodes = nodParam ? Number(nodParam) : 40
@@ -12,6 +13,7 @@ const hubFixation = fixParam ? Number(fixParam) / 100 : 0.5
 const aggressionLevel = aggParam ? Number(aggParam) / 100 : 0.5
 const numberOfHubs = 3
 const speed = speParam ? 5000 / Number(speParam) : 500
+const attackFromHub = attParam === 'true'
 let showAttacker = false
 
 // "file global" object to manage graph library, edges, nodes, hubs, and attackers
@@ -19,87 +21,79 @@ let cy
 let edges
 let nodes 
 let hubs
+let noHubNodes
 let attacker
+let globalDistances = [...Array(numberOfNodes)]
 const networkHistory = []
 
 // utils
 const genRandomInt = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min
-// https://stackoverflow.com/questions/30720103/generate-random-numbers-with-logarithmic-distribution-and-custom-slope
-const genLogDist = (zmii, max) => 
-    Math.floor(Math.log((Math.random()*(Math.pow(zmii, max)-1.0))+1.0) / Math.log(zmii));
-
-
 
 // edge creation functions (helpers)
 const genRandomTarget = source => {
     const target = genRandomInt(0, numberOfNodes - 1)
-    return target === source ? genRandomTarget(source) : target
+    return target == source ? genRandomTarget(source) : target
 }
 
 const genRandomConnection = () => {
     const source = genRandomInt(0, numberOfNodes - 1)
     const target = genRandomTarget(source)
-    return { source, target }
+    return source == target ? genRandomConnection() : { source, target }
 }
 
-const genGraphBasedConnection = (source, target) => {
-    if (hubs.length < numberOfHubs) {
-        Object.values(edges).forEach(edge => {
-            if (hubs.indexOf(edge.data.source) === -1) hubs.push(edge.data.source)
-            if (hubs.indexOf(edge.data.target) === -1) hubs.push(edge.data.target)
-        })
-        return { source, target }
-    }
-    if (hubs.indexOf(Number(source)) === -1 && Math.random() < 0.15) {
-        source = Number(hubs[Math.floor(Math.random() * hubs.length)])
-        const hubsWithoutSource = hubs.filter(hub => hub != source)
-        target = Number(hubsWithoutSource[Math.floor(Math.random() * hubsWithoutSource.length)])
-        return { source, target }
-    }
-    if (!attacker) {
-        noHubNodes = nodes.filter(node => hubs.indexOf(node.data.id) === -1)
-        attacker = nodes[genRandomInt(0, nodes.length - 1)].data.id
-        hubs.forEach(hubId => cy.$id(hubId).style({'background-color': '#73A790'}))
-        document.getElementById('show-button').classList.remove('disabled')
-    }
-    // if (Math.random() < (aggressionLevel / 5) && target != attacker) source = attacker
-    // connect to neighboring node
-    if (Math.random() < 1 - hubFixation && (source != attacker || Math.random() < 1 - aggressionLevel)) {
-        const distances = nodes.map((node, index) => {
+// get ln "random" node based on distance
+const getLnRandomNode = (nodeIds, source) => {
+    let sortedDistances
+    if(!globalDistances[source] || globalDistances[source].length === numberOfHubs) {
+        const distances = nodeIds.map((id, index) => {
+            const node = nodes[id]
             const a = node.position.x - nodes[source].position.x
             const b = node.position.y - nodes[source].position.y
             return {
-                id: index,
+                id: id,
                 distance: Math.sqrt( a*a + b*b )
             }
         })
-        const sortedDistances = distances.sort((a, b) => a.distance > b.distance)
-        sortedDistances.shift()
-        const index = lnRandomScaled(2.75, 1.5, sortedDistances.length - numberOfNodes / 5)
-        // Math.floor(Math.random() * Math.random() * Math.random() * (sortedDistances.length - numberOfNodes / 5))
-        
-        const result = { source, target: sortedDistances[index].id }
-        return result
-    } else if (source == attacker) {
-        const result = { source, target }
-        return result
+        sortedDistances = distances.sort((a, b) => a.distance > b.distance)
+        globalDistances[source] = sortedDistances
+    } else {
+        sortedDistances = globalDistances[source]
     }
-    // connect to Hub or from Hub
-    const hubsPruned = hubs.filter(element => element != source && element != target)
-    const hubsPrunedDistances = hubsPruned.map(
-        nodeId => {
-            const a = nodes[nodeId].position.x - nodes[source].position.x
-            const b = nodes[nodeId].position.y - nodes[source].position.y
-            return {
-                id: nodeId,
-                distance: Math.sqrt( a*a + b*b )
-            }
-        }
-    ).sort((a, b) => a.distance > b.distance)
-    const connectorHub = hubsPrunedDistances[0].id
-    if (Math.random() < 0.5) { source = connectorHub } else { target = connectorHub }
-    const result = { source, target }
-    return result
+    let index = Math.ceil(jStat.lognormal.sample(1, 0.5) * 0.75) - 1
+    let id
+    //const
+    if (index >= sortedDistances.length) {
+        const limit = sortedDistances.length - 1 < 5 ? sortedDistances.length - 1 : 5
+        index = genRandomInt(0, limit)
+    }
+    id = sortedDistances[index].id
+    if (id === source) {
+        sortedDistances[1].id
+    }
+    if (nodeIds.length > 10) {
+        console.log(id)
+        console.log(index)
+    }
+    return id
+}
+
+const genGraphBasedConnection = (source, target) => {
+    if(source === attacker && Math.random() < aggressionLevel) {
+        target = genRandomTarget(source)
+        return { source, target }
+    }
+    if(Math.random() < hubFixation) { // pick random Hub as source
+        source = hubs[genRandomInt(0, hubs.length - 1)]
+    }
+   if(Math.random() < hubFixation) { // pick "random" Hub as target, biased based on distance
+        target = getLnRandomNode(hubs, source)
+    } else { // pick "random" non-Hub as target, biased based on distance
+        target = getLnRandomNode(noHubNodes, source)
+    }
+    if(source == target) {
+        return genGraphBasedConnection(hubs[0], hubs[1])
+    }
+    return { source, target }
 }
 
 // core logic
@@ -153,9 +147,26 @@ const startSimulation = () => {
     )
     cy.zoomingEnabled(false)
 
-    cy.ready(() => 
+    cy.ready(() => {
+        // assign random hubs
+        hubs = Array.from(Array(numberOfHubs)).map(
+            (_, __) => Math.floor(Math.random() * numberOfNodes)
+        )
+        // set attacker
+        if (!attacker) { // determine attacker if this has not been done, yet
+            // if attacker should be hub
+            if (attackFromHub) {
+                attacker = hubs[genRandomInt(0, hubs.length - 1)]
+            } else { // if attacker should not be hub
+                potentialNodes = [...Array(numberOfNodes).keys()]
+                noHubNodes = potentialNodes.filter(id => !hubs.includes(id))
+                attacker = noHubNodes[genRandomInt(0, noHubNodes.length - 1)]
+            }
+            hubs.forEach(hubId => cy.$id(hubId).style({'background-color': '#73A790'}))
+            document.getElementById('show-button').classList.remove('disabled')
+        }
         setInterval(() => {
-            const connection = genRandomConnection()
+            let connection = genRandomConnection()
             const source = connection.source
             const target = connection.target
             const edgeArray = Object.values(edges)
@@ -186,11 +197,11 @@ const startSimulation = () => {
                 cy.add([edge])
             }
             const historyEntry = { edges, nodes }
-            networkHistory.add(historyEntry)
+            networkHistory.push(historyEntry)
             const uriContent = `data:text/plain;charset=utf-8,${encodeURIComponent(JSON.stringify(networkHistory))}`
             document.getElementById('log-button').href = uriContent
         }, speed)
-    )
+    })
 }
 
 window.addEventListener('DOMContentLoaded', _ => {
@@ -211,9 +222,11 @@ window.addEventListener('DOMContentLoaded', _ => {
 })
 
 function updateConfig (id, value) {
-    const label = document.getElementById(`${id.substring(0,3)}-tracker`)
-    const previousLabelText = label.value
-    label.value = `${previousLabelText.split(':')[0]}: ${value}`
+    try {
+        const label = document.getElementById(`${id.substring(0,3)}-tracker`)
+        const previousLabelText = label.value
+        label.value = `${previousLabelText.split(':')[0]}: ${value}`
+    } catch  (error) {}
     url.searchParams.set(id.substring(0,3), value)
     window.history.pushState({ path: url.href }, '', url.href)
 }
